c60a265afed31e2eb9b9fe42ae776af151e2ecd5
hieuonest <hieuvh@arabicatech.vn>
2024-12-06 15:58:41 +0700

    connect db

--- back-end/src/routers/index.js
+++ back-end/src/routers/index.js
@@ -2,6 +2,6 @@ import express from "express";
 import { test_api } from "../controller";
 const router = express.Router();
 
-router.use("/api/test", test_api());
+router.use("/api/test", test_api);
 
 export default router;
